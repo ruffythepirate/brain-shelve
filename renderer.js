d179eab467d42b4f41f407e297817465cbf556be
ruffy <johan.ellfries@gmail.com>
2018-05-14 21:08:19 +0200

    Add some render logic that draws a line.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+function drawALine() {
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+}
+
+drawALine();
